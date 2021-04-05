5cc876f0d51091af866b635dd1244168d701f59b
tofunmi <tofunmiseun@gmail.com>
2021-04-05 14:57:38 +0100

    ...

--- src/components/organisms/VaultDetailsMenu.js
+++ src/components/organisms/VaultDetailsMenu.js
@@ -5,6 +5,9 @@ import ModalTemplate from '../templates/ModalTemplate';
 
 
 export default function VaultDetailsMenu({ navigation, isVisible, vault, onMenuClosed = () => { } }) {
+    const [showDeleteModal, setShowDeleteModal] = React.useState(false);
+    const [deleteRecordErrorMessage, setDeleteRecordErrorMessage] = React.useState('');
+
     const goToAddRecordPage = () => {
         navigation.navigate('NewVaultRecord', { vault });
         onMenuClosed();
@@ -15,29 +18,22 @@ export default function VaultDetailsMenu({ navigation, isVisible, vault, onMenuC
         onMenuClosed();
     }
 
-    const deleteVaultPage = () => {
-
-    }
-
-    return <ModalTemplate modalVisible={isVisible} onModalDismissed={onMenuClosed} modalContent={
-        <View style={{ borderRadius: 30 }}>
-            <ListItem onPress={goToAddRecordPage}>
-                <ListItem.Content>
-                    <ListItem.Title>Add record</ListItem.Title>
-                </ListItem.Content>
-            </ListItem>
-            <Divider />
-            <ListItem onPress={goToEditPage}>
-                <ListItem.Content>
-                    <ListItem.Title>Edit</ListItem.Title>
-                </ListItem.Content>
-            </ListItem>
-            <Divider />
-            <ListItem onPress={deleteVaultPage}>
-                <ListItem.Content>
-                    <ListItem.Title style={{ color: 'red' }}>Delete</ListItem.Title>
-                </ListItem.Content>
-            </ListItem>
-        </View>}>
-    </ModalTemplate>;
+    return <>
+        <ModalTemplate modalVisible={isVisible} onModalDismissed={onMenuClosed} modalContent={
+            <View style={{ borderRadius: 30 }}>
+                <ListItem onPress={goToAddRecordPage}>
+                    <ListItem.Content>
+                        <ListItem.Title>Add record</ListItem.Title>
+                    </ListItem.Content>
+                </ListItem>
+                <Divider />
+                <ListItem onPress={goToEditPage}>
+                    <ListItem.Content>
+                        <ListItem.Title>Edit</ListItem.Title>
+                    </ListItem.Content>
+                </ListItem>
+                <Divider />
+            </View>}>
+        </ModalTemplate>
+    </>;
 }
