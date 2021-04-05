2033ab45ae6c2384f5681cc910bef9be5fe9e729
tofunmi <tofunmiseun@gmail.com>
2021-04-05 15:36:07 +0100

    added functionality to visit external link

--- src/pages/VaultRecordDetailsPage.js
+++ src/pages/VaultRecordDetailsPage.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, Linking } from 'react-native';
 import { EvilIcons, Feather, Ionicons } from '@expo/vector-icons';
 import Clipboard from 'expo-clipboard';
 import ListItemSeparator from '../components/atoms/ListItemSeparator';
@@ -15,6 +15,17 @@ export default function VaultRecordDetails({ route, navigation }) {
         return "*".repeat(vaultRecord.password.length);
     }
 
+    const openRecordUrl = () => {
+        if (Boolean(vaultRecord.url)) {
+            let urlToVisit = vaultRecord.url.toLowerCase();
+            if (!urlToVisit.startsWith('http://') && !urlToVisit.startsWith('https://')) {
+                urlToVisit = 'https://' + urlToVisit;
+            }
+            Linking.openURL(urlToVisit).catch((err) => { });
+        }
+
+    }
+
     const copyToClipboard = (text) => {
         Clipboard.setString(text);
         setShowClipboardAlert(true);
@@ -22,14 +33,15 @@ export default function VaultRecordDetails({ route, navigation }) {
     }
 
     return (
-        <View style={DetailsPageStyles.section}>
-            <View>
+        <View style={{ flex: 1, backgroundColor: 'white' }}>
+            <View style={DetailsPageStyles.section}>
                 {Boolean(vaultRecord.url) && <View style={DetailsPageStyles.formContainer}>
                     <Text style={DetailsPageStyles.formLabel}>url</Text>
                     <View style={DetailsPageStyles.formDataContainer}>
                         <Text style={DetailsPageStyles.formValue}>{vaultRecord.url}</Text>
-                        <TouchableOpacity style={{ marginTop: 'auto', marginBottom: 'auto', marginLeft: 'auto' }}>
-                            <EvilIcons name="external-link" size={24} color="black" />
+                        <TouchableOpacity onPress={openRecordUrl}
+                            style={{ marginTop: 'auto', marginBottom: 'auto', marginLeft: 'auto', }}>
+                            <EvilIcons name="external-link" size={28} color="black" />
                         </TouchableOpacity>
                     </View>
                 </View>}
@@ -40,7 +52,7 @@ export default function VaultRecordDetails({ route, navigation }) {
                         <Text style={DetailsPageStyles.formValue}>{vaultRecord.username}</Text>
                         <TouchableOpacity onPress={() => copyToClipboard(vaultRecord.username)}
                             style={{ marginTop: 'auto', marginBottom: 'auto', marginLeft: 'auto' }}>
-                            <Feather name="copy" size={16} color="black" />
+                            <Feather name="copy" size={20} color="black" />
                         </TouchableOpacity>
                     </View>
                 </View>
@@ -54,14 +66,15 @@ export default function VaultRecordDetails({ route, navigation }) {
                         <TouchableOpacity onPress={() => setPasswordRevealed(!isPasswordRevealed)}
                             style={{ marginTop: 'auto', marginLeft: 16 }}>
                             <Ionicons name={isPasswordRevealed ? 'ios-eye-off' : 'ios-eye'}
-                                size={16} color="black" />
+                                size={20} color="black" />
                         </TouchableOpacity>
                         <TouchableOpacity onPress={() => copyToClipboard(vaultRecord.password)}
                             style={{ marginTop: 'auto', marginBottom: 'auto', marginLeft: 'auto' }}>
-                            <Feather name="copy" size={16} color="black" />
+                            <Feather name="copy" size={20} color="black" />
                         </TouchableOpacity>
                     </View>
                 </View>
+                <View style={DetailsPageStyles.formContainer}><ListItemSeparator /></View>
             </View>
             {showClipboardAlert && <Alert message={'Text copied to clipboard!'}
                 onClosed={() => setShowClipboardAlert(false)} />}
